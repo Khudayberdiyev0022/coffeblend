fe23d04fab96eb53e368d96d48850ae9844d2f03
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-01-24 20:02:01 +0500

    git commit "all commit"

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,38 +1,42 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import {FiShoppingCart} from 'react-icons/fi'
+import './styles/navbar.css'
 
 class Navbar extends Component {
     render() {
         return (
-            <div className='container-fluid'>
-                <nav class="navbar navbar-expand-md navbar-light bg-light">
-                    <nav-link class="navbar-brand" href="#"><Link> Navbar </Link></nav-link>
-                    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                        aria-expanded="false" aria-label="Toggle navigation">
+            <div className='Navbar container-fluid'>
+                <nav class="navbar navbar-expand-md navbar-light nav-item">
+                    <Link> Navbar </Link>
+                    <button class="navbar-toggler d-lg-none" type="button">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="collapsibleNavId">
-                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <li class="nav-item active">
-                                <nav-link class="nav-link" href="#"><Link to='/'> Home </Link></nav-link>
+                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                            <li class="nav-item">
+                                <Link to='/'> Home </Link>
                             </li>
                             <li class="nav-item">
-                                <nav-link class="nav-link" href="#"><Link to='/menu'> Menu </Link></nav-link>
+                             <Link to='/menu'> Menu </Link>
                             </li>
                             <li class="nav-item">
-                                <nav-link class="nav-link" href="#"><Link to='/services'> Services </Link></nav-link>
+                                <Link to='/services'> Services </Link>
                             </li>
                             <li class="nav-item">
-                                <nav-link class="nav-link" href="#"><Link to='/blog'> Blog </Link></nav-link>
+                                <Link to='/blog'> Blog </Link>
                             </li>
                             <li class="nav-item">
-                                <nav-link class="nav-link" href="#"><Link to='/about'> About </Link></nav-link>
+                                <Link to='/about'> About </Link>
                             </li>
                             <li class="nav-item dropdown">
-                                <nav-link class="nav-link" href="#"><Link to='/shop'> Shop </Link></nav-link>
+                                <Link to='/shop'> Shop </Link>
                             </li>
                              <li class="nav-item">
-                                <nav-link class="nav-link" href="#"><Link to='/contact'> Contact </Link></nav-link>
+                                <Link to='/contact'> Contact </Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to='/shoppingcard'> <FiShoppingCart /> </Link>
                             </li>
                         </ul>
                     </div>
