6997090cd31ce697d1afcaec65596402ebbf6b78
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-01-26 01:02:01 +0500

    "test"

--- src/components/Home3.js
+++ src/components/Home3.js
@@ -8,19 +8,19 @@ class Home3 extends Component {
         return (
             <div className='Home3' style={{ backgroundColor: `${color}` }}>
                 <div className="form-group" style={{ backgroundColor: `${color}` }}>
-                    <h3 className='text-light m-1'>Book a table</h3>
-                    <div className='input_top d-flex'>
-                        <input type="text" className="form-control m-1" placeholder="First Name" />
-                        <input type="text" className="form-control m-1" placeholder="Last Name" />
+                    <h6 className='m-1'>Book a table</h6>
+                    <div className='d-flex'>
+                        <input type="text" className="form-control" placeholder="First Name" />
+                        <input type="text" className="form-control" placeholder="Last Name" />
                     </div>
-                    <div className='input_top d-flex'>
-                        <input type="date" className="form-control m-1" />
-                        <input type="time" className="form-control m-1" />
-                        <input type="tel" className="form-control m-1" placeholder="Phone" />
+                    <div className='d-flex'>
+                        <input type="date" className="form-control" />
+                        <input type="time" className="form-control" />
+                        <input type="tel" className="form-control" placeholder="Phone" />
                     </div>
-                    <div className='input_top d-flex'>
-                        <textarea className="form-control m-1" cols="70" rows="1" placeholder="Message"></textarea>
-                        <input type="submit" className="form-control m-1 " value="Appointment" id="btn" />
+                    <div className='d-flex'>
+                        <textarea className="form-control" placeholder="Message"></textarea>
+                        <input type="submit" className="form-control m-1 p-2" value="Appointment" id="btn" />
                     </div>
                 </div>
             </div>
